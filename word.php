4bc2993642cc8b108e69f6aa926f78aa285111e4
CreatiVe <naukomania@gmail.com>
2019-05-08 01:30:40 +0700

    change word.php

--- word.php
+++ word.php
@@ -1,20 +1,31 @@
 <?php
-$lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'RUS';
-$words = [
-'о нас' => 'about',
-'услуги' => 'service',
-'наша команда' => 'our team',
-'портфолио' => 'portfolio',
-];
+function getGlobals() {
+  $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'RUS';
+  $words = [
+    'о нас' => 'about',
+    'услуги' => 'service',
+    'наша команда' => 'our team',
+    'портфолио' => 'portfolio',
+  ];
+
+  return [
+    'lang' => $lang,
+    'words' => $words,
+  ];
+}
 
 function t($word) {
-    if($lang == 'RUS') {
+  $ar = getGlobals();
+  $lang = $ar['lang'];
+  $words = $ar['words'];
+
+  if($lang == 'RUS') {
     return $word;
-    }
+  }
     
-    if(isset($words[$word])) {
+  if(isset($words[$word])) {
     return $words[$word];
-    }
+  }
     
-    return 'Перевод фразы не установлен';
-    }
+  return 'Перевод фразы не установлен';
+}
