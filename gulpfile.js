bcc43349f9f1a70551913c72f893723c5daa88a6
Mariya <Mariyashabrova@yandex.ru>
2019-06-04 20:25:25 +0300

    merge commit

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,8 @@ const banner = ['/*!\n',
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./"
+      //baseDir: "./"
+      proxy: "localhost:8000"
     },
     port: 3000
   });
